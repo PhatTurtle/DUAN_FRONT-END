1fc2d3eb6b925f94e62ebc27da3598115978bcbb
tai2319 <99863150+tai2319@users.noreply.github.com>
2023-09-24 15:24:11 +0700

    tai

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Button/Login/Login";
 import Header from "./Components/Header/Header";
+import Register from "./Components/Button/Register/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Header />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
