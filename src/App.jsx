2585e46ff74dc086f7735f92d3ab525fc088beaf
PhatTurtle <phatrua2911@gmail.com>
2023-09-24 14:42:43 +0700

    phat-menu-category

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./Pages/HomePage/HomePage";
 
 
 function App() {
-  return <></>;
+  return (
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+  );
 }
 
 export default App;
