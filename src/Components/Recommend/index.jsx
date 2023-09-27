a5ce6f11701b2796f4b41e7ee2b0cb945b6efb0c
PhatTurtle <phatrua2911@gmail.com>
2023-09-27 14:01:13 +0700

    phat

---
+++ src/Components/Recommend/index.jsx
@@ -0,0 +1,30 @@
+function Recommend() {
+    return (  
+
+        <>
+            <section className="container-fluid bg-dark">
+                <div className="row d-flex justify-content-start">
+                    <img src="armchair.png" alt="" />
+                    <div>
+                        <span>Phúc Như CC</span>
+                        <a href="#" className="btn btn-outline-primary">Khám Phá Ngay</a>
+                    </div>
+                </div>
+                <div className="row d-flex justify-content-end">
+                    <div>
+                        <span>Phúc Như CC</span>
+                        <a href="#" className="btn btn-outline-primary">Khám Phá Ngay</a>
+                    </div>
+                    <img src="armchair.png" alt="" />
+                </div>
+            </section>
+        
+        
+        </>
+
+
+
+    );
+}
+
+export default Recommend;
