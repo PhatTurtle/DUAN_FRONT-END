94b0a7e3da8d7b0bd942b5a4dd48845628abd036
PhatTurtle <phatrua2911@gmail.com>
2023-09-27 13:07:14 +0700

    phat button

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -4,13 +4,17 @@ const Product = () => {
 
     return (
         <>
-        <div class="card border-light mb-3 hovers" style={{width: "250px"}}>
+        <div class="card border-light mb-3 hovers" style={{width: "260px"}}>
             <img style={{scale:'.4'}} src="armchair.png" class="card-img-top" alt="..."/>
             <hr />
             <div class="card-body">
                 <h5 class="card-title">Phúc EX</h5>
                 <p class="card-text">Phúc như đuồi bầu</p>
-                <a href="#" class="btn btn-outline-danger">Mua Ngay</a>
+                <div className="d-flex justify-content-between">
+                    <a href="#" class="btn none btn-outline-warning ">Mua Ngay</a>
+                    <a href="#" class="btn none btn-outline-danger">Mua Ngay</a>
+                </div>
+                
             </div>
         </div>
         </>
