8549a7702aa08c4981ac886a870eac0c5eb70bb1
PhatTurtle <phatrua2911@gmail.com>
2023-09-30 15:16:23 +0700

    phat

--- src/Components/Recommend/index.jsx
+++ src/Components/Recommend/index.jsx
@@ -11,11 +11,11 @@ function Recommend() {
                     </div>
                 </div>
                 <div className=" d-flex justify-content-end">
-                    <div className="d-flex flex-column align-self-center">
-                        <span>Phúc Như CC</span>
-                        <a href="#" className="btn btn-outline-primary">Khám Phá Ngay</a>
+                    <div style={{}} className="d-flex flex-column align-self-center mx-5">
+                        <span style={{maxWidth:'50%'}} className="my-4">Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.</span>
+                        <a style={{maxWidth:'20%'}} href="#" className="btn btn-outline-primary ">Khám Phá Ngay</a>
                     </div>
-                    <img style={{width:'20%'}} src="armchair.png" alt="" />
+                    <img style={{maxWidth:'20%'}} src="armchair.png" alt="" />
                 </div>
             </section>
         
