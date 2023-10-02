2bdd4359f72ee3921d09c18a6705fdbf4f035cf5
PhatTurtle <phatrua2911@gmail.com>
2023-10-02 08:59:27 +0700

    phat

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,24 +1,46 @@
+import { useEffect, useState } from "react";
 import "./Product.css"
+import axios from "axios";
+
 
 const Product = () => {
+     const [apiProduct, setapiProduct] = useState([])
+     function handle (data){
+            setapiProduct(data)
+    }
+     useEffect(() =>{
+          axios.get('https://dummyjson.com/products')
+          .then((resp) =>{
+            handle(resp.data)
+          })
+            
+  
+     },[])
 
+    
     return (
-        <>
-        <div class="card border-light mb-3 hovers" style={{width: "260px"}}>
-            <img style={{scale:'.4'}} src="armchair.png" class="card-img-top img-pro" alt="..."/>
-            <hr />
-            <div class="card-body">
-                <h5 class="card-title">Phúc EX</h5>
-                <p class="card-text">Phúc như đuồi bầu</p>
-                <div className="d-flex justify-content-between">
-                    <a href="#" class="btn none btn-outline-warning ">Mua Ngay</a>
-                    <a href="#" class="btn none btn-outline-danger">Mua Ngay</a>
-                </div>
-                
-            </div>
-        </div>
-        </>
+        <section>
         
+        {
+          
+            apiProduct.map((datas) =>(
+                <div class="card border-light mb-3 hovers" key={datas.id} style={{width: "260px"}}>
+                    <img style={{scale:'.4'}} src="armchair.png" class="card-img-top img-pro" alt="..."/>
+                    <hr />
+                    <div class="card-body">
+                        <h5 class="card-title">{datas.title}</h5>
+                        <p class="card-text">{datas.description}</p>
+                        <div className="d-flex justify-content-between">
+                            <a href="#" class="btn none btn-outline-warning ">Mua Ngay</a>
+                            <a href="#" class="btn none btn-outline-danger">Mua Ngay</a>
+                        </div>
+                    </div>
+                </div>
+            ))
+        }
+        
+        
+        </section>
     )
 }
 
--- src/Components/Recommend/index.jsx
+++ src/Components/Recommend/index.jsx
@@ -2,22 +2,35 @@ function Recommend() {
     return (  
 
         <>
-            <section className=" bg-dark text-white">
-                <div className="d-flex justify-content-start">
-                    <img style={{width:'20%'}}  src="armchair.png" alt="" />
-                    <div className="d-flex flex-column align-self-center">
-                        <span>Phúc Như CC</span>
-                        <a href="#" className="btn btn-outline-primary">Khám Phá Ngay</a>
+        <section className=" bg-dark text-white">
+        <section className=" container">
+                <div className="row pt-5">
+                    <div className="col-lg-4">
+                        <img  src="armchair.png" style={{width:'40%'}} alt="" />
                     </div>
+                    <div className="col-lg-6 d-flex flex-column justify-content-center" style={{width:'60%'}}>
+                        <p style={{fontSize:'small'}}>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.</p>
+                        <button style={{width:'30%'}}>Khám Phá Ngay</button>
+                    </div>
+                
                 </div>
-                <div className=" d-flex justify-content-end">
-                    <div style={{}} className="d-flex flex-column align-self-center mx-5">
-                        <span style={{maxWidth:'50%'}} className="my-4">Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.</span>
-                        <a style={{maxWidth:'20%'}} href="#" className="btn btn-outline-primary ">Khám Phá Ngay</a>
+                <br />
+                <div className="row mt-5">
+                    
+                    <div className="col-lg-8 d-flex flex-column justify-content-center " style={{width:'60%'}}>
+                        <p className="" style={{fontSize:'small'}}>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.</p>
+                        <button className="" style={{width:'30%'}}>Khám Phá Ngay</button>
+                    </div>   
+                    
+                    <div className="col-lg-4 d-flex justify-content-end">
+                        <img className="" src="armchair.png" style={{width:'40%'}} alt="" />
                     </div>
-                    <img style={{maxWidth:'20%'}} src="armchair.png" alt="" />
+                    
+                    
                 </div>
             </section>
+        </section>
+            
         
         
         </>
